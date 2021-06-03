9d521a0d9b303b79d1242433079f66c0f993013c
nitishthorat <nitishthorat112@gmail.com>
2021-06-03 15:14:42 +0530

    Subtract implementation

--- math.py
+++ math.py
@@ -7,8 +7,8 @@ def subtract(x,y):
     if y > x:
         return ERROR                #Main implementation
     else: 
-        return x - y
-    
+        return x - y            
+
 #Multiplication implementation
 def multiply(x,y):
     pass
